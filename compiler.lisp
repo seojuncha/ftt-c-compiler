b0939a53ddb2d04d80b8f1d35e745c8b022fe39e
Seojun Cha <sjcha36@gmail.com>
2025-07-07 22:29:00 +0900

    just sync

--- compiler.lisp
+++ compiler.lisp
@@ -1,23 +1,92 @@
-(defparameter *tok-kind*
-  '(:tok-identifier
-    :tok-numeric-constant))
-
 (defparameter *puctuator*
   '(:tok-plus
     :tok-minus))
 
+(defparameter *tok-kind*
+  (append
+      ; identifier
+      ; i.e) abc
+    '(:tok-identifier
+
+      ; numeric constant(integer, float), 
+      ; i.e) 123, 1.2
+      :tok-numeric-constant
+
+      ; string literal
+      ; i.e) "abc"
+      :tok-string-literal
+
+      ; unkown token 
+      :tok-unknown
+
+      ; end of file
+      :tok-eof)
+    *puctuator*))
+
 (defparameter *plus-expr* "1+2")
+;(defparameter *plus-expr* "12+2")
+;(defparameter *plus-expr* "12+23")
 
-(defun parse-expression (expr)
-  (format t "EXPR: ~s~%" expr)
-  (let ((lhs (parse-assignment-expression expr)))
-    (format t "LHS: ~a~%" lhs)))
+;;; managing tokens
+;;; current token symbol pointed from *ptr*
+(defparameter *cur-tok* nil)
+;;; lookahead token symbol
+(defparameter *lookahead-tok* nil)
+;; points to the current token index
+(defparameter *buf-ptr* nil)
 
-(defun parse-assignment-expression (expr))
+(defclass token ()
+  ((kind
+    :initarg :kind
+    :initform :tok-unknown)
+   (lexeme
+    :initarg :lexeme
+    :initform "")))
+
+(defgeneric dump-token (obj))
+(defmethod dump-token ((obj token)))
+
+(defun create-token (lexeme tok-kind)
+  (make-instance 'token :lexeme lexeme :kind tok-kind))
+
+;;; character utilities
+
+;; read one character by using *buf-ptr*
+(defun read-one-char ()
+  (char *plus-expr* *buf-ptr*)
+  (incf *buf-ptr*))
+
+(defun read-numeric-char ()
+  (read-one-char))
+
+;;; lexing functions
+(defun init-lexer ()
+  (format t "initialize the lexer~%")
+  (setq *buf-ptr* 0))
+
+;; return a token?
+(defun lex ())
+
+(defun next-token ())
+
+;;; parsing functions
+(defun parse-expression ()
+  (format t "TEST EXPR: ~s~%" *plus-expr*)
+  (let ((lhs (parse-assignment-expression)))
+    (format t "LHS: ~s~%" lhs)
+    (parse-rhs-of-binary-expression lhs)))
+
+(defun parse-assignment-expression ()
+  (let ((tok (next-token)))
+    (format t "tok: ~s~%" tok)
+    (setq *cur-tok* tok)))
 
 
 (defun parse-rhs-of-binary-expression (lhs))
 
+(defun parse-numeric-constant (str))
+
 
 (format t "Start parsing~%")
-(format t "result: ~a~%" (parse-expression *plus-expr*))
+(init-lexer)
+(format t "result: ~a~%" (parse-expression))
