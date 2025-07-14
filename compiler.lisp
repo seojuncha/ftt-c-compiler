4f4ee81037012501abfa6b86d52b017a48f454cf
Seojun Cha <sjcha36@gmail.com>
2025-07-14 20:52:15 +0900

    lex a numeric constant

--- compiler.lisp
+++ compiler.lisp
@@ -7,25 +7,32 @@
       ; identifier
       ; i.e) abc
     '(:tok-identifier
+
       ; numeric constant(integer, float),
       ; i.e) 123, 1.2
       :tok-numeric-constant
+
       ; string literal
       ; i.e) "abc"
       :tok-string-literal
+
       ; unkown token
       :tok-unknown
+
       ; end of file
       :tok-eof)
     *puctuator*))
 
 ;; Success
-(defparameter *test-expr* "5+2")
+; (defparameter *test-expr* "5+2")
 ; (defparameter *test-expr* "5-2")
 
-;; TODO
-;(defparameter *test-expr* "12+2")
-;(defparameter *test-expr* "12+23")
+;; working
+(defparameter *test-expr* "232-5010")
+
+;; todo
+; (defparameter *test-expr* "12+23")
+
 
 ;;; ----------  Token
 (defclass token ()
@@ -60,11 +67,16 @@
 (defparameter *buf-end* nil)
 
 ;;; ----------  character utilities
-;; read one character, return the incremented pointer.
-(defun consume-char (size)
-  (format t "buffer pointer: [~d] ~s~%" *buf-ptr* (char *test-expr* *buf-ptr*))
-  ; todo: use a buffer instead of *plus-expr* to store code
-  (char *test-expr* *buf-ptr*))
+;; read one character with the incremented pointer.
+(defun consume-char (ptr)
+  ; todo: use a buffer instead of *test-expr* to store code
+  (when (< ptr *buf-end*)
+    (values (char *test-expr* ptr) (incf ptr))))
+
+; read just a chracter. not modify the pointer
+(defun get-char (ptr)
+  (when (< ptr *buf-end*)
+    (char *test-expr* ptr)))
 
 ;;; ---------- Lexer
 (defun init-lexer ()
@@ -72,20 +84,33 @@
   (setq *buf-start* 0)
   (setq *buf-end* (length *test-expr*))) ; length, fixed now.
 
-(defun form-token-with-chars (lexeme tok-kind)
-  ; (format t "CREATE NEW TOKEN~%")
-  (make-instance 'token :lexeme lexeme :kind tok-kind))
+(defun form-token-with-chars (tokend tok-kind)
+  (let ((toklen (- tokend *buf-ptr*))
+        (lexeme nil))
+    (format t " TOKLEN: ~d~%" toklen)
+    (setf lexeme (subseq *test-expr* *buf-ptr* (+ *buf-ptr* toklen)))
+    (setf *buf-ptr* tokend)
+    (format t "buffer pointer after creating token: ~d~%" *buf-ptr*)
+    (format t "CREATE TOKEN: ~a | ~s~%" tok-kind lexeme)
+    (make-instance 'token
+      :lexeme lexeme
+      :kind tok-kind)))
 
 ;; start to lex, return a token
 (defun lex ()
-  ;; todo: loop here! until finding a token lexeme not a single character.
   (when (< *buf-ptr* *buf-end*)
-    (let ((c (consume-char 1))
+    (let ((cur-ptr nil)
+          (c nil)
           (tok-kind nil))
-      ; (format t "   current buffer pointer: ~d~%" *buf-ptr*)
-      (incf *buf-ptr*)
-      (cond ((and (char> c #\0) (char< c #\9))
-              (setq tok-kind :tok-numeric-constant))
+      (setf cur-ptr *buf-ptr*)
+      (format t "buffer pointer: ~d~%" *buf-ptr*)
+      ; read the first character from the current pointer.
+      (setf c (get-char cur-ptr))
+      ; points to the next character.
+      (incf cur-ptr)
+      (format t "first character: ~s~%" c)
+      (cond ((digit-char-p c)
+              (return-from lex (lex-numeric-constant cur-ptr)))
             ((char= c #\+)
               (setq tok-kind :tok-plus))
             ((char= c #\-)
@@ -93,11 +118,22 @@
             (t
               (format t "unkonwn: ~s~%" c)
               (setq tok-kind :tok-unkonwn)))
-      (form-token-with-chars c tok-kind))))
+      (form-token-with-chars cur-ptr tok-kind))))
 
 ;; lexing the continuous numeric characters.
-(defun lex-numeric-constant ()
-  (format t "lex-numeric-constant~%"))
+(defun lex-numeric-constant (curptr)
+  (format t "lex-numeric-constant~%")
+  (let ((firstchar (get-char curptr)))
+    (loop
+      with ptr = curptr
+      for ch = firstchar then (when (< ptr *buf-end*) (get-char ptr))
+      while (and (< ptr *buf-end*) (digit-char-p ch)) do
+        (format t "before consume; p: ~d ch: ~s~%" ptr ch)
+        (multiple-value-bind (c p) (consume-char ptr)
+          (format t "after consume p: ~d, ch: ~s~%" p c)
+          (setf ptr p)
+          (setf curptr p))))
+  (form-token-with-chars curptr :tok-numeric-constant))
 
 ;;; ---------- Parser
 (defun init-parser ()
@@ -115,38 +151,43 @@
   (parse-expression))
 
 (defun parse-expression ()
-  ; (format t "parse-expression~%")
+  (format t "parse-expression~%")
   (let ((lhs (parse-assignment-expression)))
     ; (format t "LHS 00: ")
     ; (dump-ast lhs)
     (parse-rhs-of-binary-expression lhs)))
 
 (defun parse-cast-expression ()
-  ; (format t "parse-cast-expression~%")
-  (let ((saved-kind (tok-kind *cur-tok*)))
+  (format t "parse-cast-expression~%")
+  (let ((saved-kind (tok-kind *cur-tok*))
+        (result nil))
     (cond ((eq saved-kind :tok-numeric-constant)
-            (consume-token) (create-ast-integer-literal))
+            (setf result (create-ast-integer-literal (tok-lexeme *cur-tok*)))
+            (consume-token)
+            result)
           ((or (eq saved-kind :tok-plus) (eq saved-kind :tok-minus))
             (consume-token))
           (t
             (format t "unknown token: ~a~%" saved-kind)))))
 
 (defun parse-assignment-expression ()
-  ; (format t "parse-assignment-expression~%")
+  (format t "parse-assignment-expression~%")
   (let ((lhs (parse-cast-expression)))
     ; (format t "LHS 11: ")
     ; (dump-ast lhs)
     (parse-rhs-of-binary-expression lhs)))
 
 (defun parse-rhs-of-binary-expression (lhs)
-  ; (format t "parse-rhs-of-binary-expression~%")
+  (format t "parse-rhs-of-binary-expression~%")
   (let ((optok nil)
         (rhs nil))
     ; (format t "LHS 22: ")
     ; (dump-ast lhs)
+
     ; temp: should calculate the precedence of operators.
     (when (= *buf-ptr* *buf-end*)
       (return-from parse-rhs-of-binary-expression lhs))
+
     ; will(and must) be an operator.
     (setf optok *cur-tok*)
     ; (dump-token optok)
@@ -183,10 +224,13 @@
   (make-instance 'ast-binary-operator :opkind opkind :lhs lhs :rhs rhs))
 
 (defclass ast-integer-literal ()
-  ())
+  ((value
+   :initarg :value
+   :initform nil
+   :accessor value)))
 
-(defun create-ast-integer-literal ()
-  (make-instance 'ast-integer-literal))
+(defun create-ast-integer-literal (value)
+  (make-instance 'ast-integer-literal :value value))
 
 (defgeneric dump-ast (obj))
 (defmethod dump-ast ((obj ast-binary-operator))
@@ -194,11 +238,13 @@
     (format t "AST: ~a~%" obj)
     (format t "  KIND: ~a~%" (op-kind obj))
     (format t "  LHS: ~a~%" (lhs obj))
-    (format t "  RHS: ~a~%" (rhs obj))))
-
+    (dump-ast (lhs obj))
+    (format t "  RHS: ~a~%" (rhs obj))
+    (dump-ast (rhs obj))))
 (defmethod dump-ast ((obj ast-integer-literal))
   (when obj
-    (format t "AST: ~a~%" obj)))
+    (format t "  AST: ~a~%" obj)
+    (format t "    VALUE: ~a~%" (value obj))))
 
 ;;; ---- compiler
 (format t "start parsing: ~s~%~%" *test-expr*)
